fcc85717b8a2b02615203a7ed5a6595c91a7d43e
Carlos Rubio <calitod91@gmail.com>
2023-07-01 19:42:59 -0600

    Documentando codigo

--- js/perceptron.js
+++ js/perceptron.js
@@ -1,13 +1,38 @@
 const letter = document.getElementById('letter');
 const inputs = document.getElementsByClassName('inputs');
 
+// const modelOutputs = [
+//     [1, -1, -1, -1, -1, -1, -1],
+//     [-1, 1, -1, -1, -1, -1, -1],
+//     [-1, -1, 1, -1, -1, -1, -1],
+//     [-1, -1, -1, 1, -1, -1, -1],
+//     [-1, -1, -1, -1, 1, -1, -1],
+//     [-1, -1, -1, -1, -1, 1, -1],
+//     [-1, -1, -1, -1, -1, -1, 1]
+// ]
+
+const outputs = {
+    '0': 'A',
+    '1': 'B',
+    '2': 'C',
+    '3': 'D',
+    '4': 'E',
+    '5': 'J',
+    '6': 'K',
+    '-1': 'Desconocido'
+}
+
+// Declaracion de variables
+const arrEntrada = Array(63) // Arreglo de entrada, El arreglo de las letras entradas por el usuario
+const output = [] // Arreglo de la salida final que determinara el tipo de letra
+let epoch = 0 // Las epocas que se tarda el algoritmo en aprender
+// const cont = 0
+
 // Data de entrenamiento
-const trainingDataList = data;
-const letters = 'ABCDEJK'
+const trainingDataList = data; // Data de entrenamiento
 
 // Funcion de activacion
 const activationFunc = (y_in, tetta = 0) => {
-    // console.log({ y_in })
     if (y_in > tetta) {
         return 1
     } else if (-tetta <= y_in && y_in <= tetta) {
@@ -28,33 +53,11 @@ const setWeight = (num) => {
 }
 
 // Calcular error
-const calcError = (error, total) => (error / total) * 100
-
-const modelOutputs = [
-    [1, -1, -1, -1, -1, -1, -1],
-    [-1, 1, -1, -1, -1, -1, -1],
-    [-1, -1, 1, -1, -1, -1, -1],
-    [-1, -1, -1, 1, -1, -1, -1],
-    [-1, -1, -1, -1, 1, -1, -1],
-    [-1, -1, -1, -1, -1, 1, -1],
-    [-1, -1, -1, -1, -1, -1, 1]
-]
-
-const modelOutputs2 = {
-    '0': 'A',
-    '1': 'B',
-    '2': 'C',
-    '3': 'D',
-    '4': 'E',
-    '5': 'J',
-    '6': 'K',
-    '-1': 'Desconocido'
-}
+// const calcError = (error, total) => (error / total) * 100
 
 // Inicializamos los pesos y biases en 0
 const arrWeights = setWeight(7)
 let errors = [true]
-let epoch = 0
 
 // ***** TRAINING PHASE *****
 const entrenar = () => {
@@ -64,30 +67,33 @@ const entrenar = () => {
 
         epoch += 1 // Aumentamos en 1 las epocas
 
-        // console.log(weights)
+        // Recorremos la data de entrenamiento
         for (let data of trainingDataList) {
+            // Separamos los primeros 63 elementos de la matriz que conforman cada letra y la ultima posicion pertenece al bias
             x = data.slice(0, 64)
+            // Data esperada o t
             let expected = data.slice(-7)
 
+            // Recorremos la matriz de pesos que es de 7 * 63
             for (let i = 0; i < weights.length; i++) {
-                const weight = weights[i]
-                const t = expected[i]
-                let result = 0
+                const weight = weights[i] // Obtenemos cada peso por individual de la matriz de pesos (Obtendra un nuevo arreglo de 63 elementos)
+                const t = expected[i] // Cada item de t que es el valor esperado
+                let result = 0 // La sumatoria de los pesos por x que es la entrada, inicialmente sera 0
 
-                // console.log("WEIGHT, X ",weight, x)
+                // Recorremos cada elemento de el arreglo de pesos (los 63 pesos individuales)
                 for (let j = 0; j < weight.length; j++) {
-                    const w = weight[j]
-                    const s = x[j]
-                    // console.log('W, S ', w, s)
+                    const w = weight[j]  // Cada peso se asigna a una variable x
+                    const s = x[j] 
 
-                    result += w * s
+                    result += w * s // Va almacenando la sumatoria
                 }
 
-                result += weight[63]
+                result += weight[63] // A la sumatoria se le suma el bias
 
+                // La funcion de activacion en este caso es la que determina el valor de y_in
                 if (activationFunc(result) != t) {
                     for (let pos = 0; pos < 63; pos++) {
-                        weight[pos] += t * x[pos]
+                        weight[pos] += t * x[pos] // Actualizar el peso
                     }
 
                     weight[63] += t // Actualizar bias
@@ -102,10 +108,7 @@ const entrenar = () => {
     console.log({epoch})
 }
 
-const arrEntrada = Array(63)
-const output = []
-const cont = 0
-
+// Repie el mismo procedimiento de aprendizaje omitiendo los pasos de actualizacion de los pesos y el biass
 const evaluar = () => {
     output.length = 0
     arrEntrada.length = []
@@ -118,12 +121,9 @@ const evaluar = () => {
         for(let j = 0; j < arrEntrada.length; j++){
             result +=  arrEntrada[j] * arrWeights[i][j];
         }
+
         let y = activationFunc(result);
         output.push(y)
     }
-    letter.innerHTML = modelOutputs2[output.indexOf(1).toString()]
-    // console.log(arrEntrada)
-    // console.log(modelOutputs2[output.indexOf(1).toString()])
-    // console.log(output.indexOf(1).toString())
-    // console.log(output)
+    letter.innerHTML = outputs[output.indexOf(1).toString()]
 }
