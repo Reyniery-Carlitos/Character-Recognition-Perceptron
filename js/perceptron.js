b24c6db9ba01a8fd62a18cf8b671a84931307cd6
Carlos Rubio <calitod91@gmail.com>
2023-07-05 21:13:58 -0600

    Actualizando a tema oscuro

--- js/perceptron.js
+++ js/perceptron.js
@@ -11,19 +11,31 @@ const inputs = document.getElementsByClassName('inputs');
 //     [-1, -1, -1, -1, -1, -1, 1]
 // ]
 
+// const outputs = {
+//     '0': 'A',
+//     '1': 'B',
+//     '2': 'C',
+//     '3': 'D',
+//     '4': 'E',
+//     '5': 'J',
+//     '6': 'K',
+//     '-1': 'Desconocido'
+// }
+
 const outputs = {
-    '0': 'A',
-    '1': 'B',
-    '2': 'C',
-    '3': 'D',
-    '4': 'E',
-    '5': 'J',
-    '6': 'K',
-    '-1': 'Desconocido'
+    1: 'A',
+    2: 'B',
+    3: 'C',
+    4: 'D',
+    5: 'E',
+    6: 'J',
+    7: 'K',
+    0: 'Desconocida'
 }
 
+
 // Declaracion de variables
-const arrEntrada = Array(63) // Arreglo de entrada, El arreglo de las letras entradas por el usuario
+const inputArr = Array(63) // Arreglo de entrada, El arreglo de las letras entradas por el usuario
 const output = [] // Arreglo de la salida final que determinara el tipo de letra
 let epoch = 0 // Las epocas que se tarda el algoritmo en aprender
 // const cont = 0
@@ -32,7 +44,7 @@ let epoch = 0 // Las epocas que se tarda el algoritmo en aprender
 const trainingDataList = data; // Data de entrenamiento
 
 // Funcion de activacion
-const activationFunc = (y_in, tetta = 0) => {
+const activationFunc = (y_in, tetta = 1.2) => {
     if (y_in > tetta) {
         return 1
     } else if (tetta >= y_in && y_in >= -tetta) {
@@ -46,14 +58,14 @@ const activationFunc = (y_in, tetta = 0) => {
 const setWeight = (num) => {
     weights = []
     for (let i = 0; i < num; i++) {
-        weights[i] = Array(64).fill(Math.random())
-        // weights[i] = Array(64).fill(0)
+        // weights[i] = Array(64).fill(Math.random())
+        weights[i] = Array(64).fill(0)
     }
     return weights
 }
 
 // Inicializamos los pesos y biases en 0
-const arrWeights = setWeight(7)
+const arrWeights = setWeight(5)
 let errors = [true]
 
 // ***** TRAINING PHASE *****
@@ -69,11 +81,11 @@ const entrenar = () => {
             // Separamos los primeros 63 elementos de la matriz que conforman cada letra y la ultima posicion pertenece al bias
             x = data.slice(0, 64)
             // Data esperada o t
-            let expected = data.slice(-7)
+            let expected = data.slice(-5)
 
             // Recorremos la matriz de pesos que es de 7 * 63
-            for (let i = 0; i < weights.length; i++) {
-                const weight = weights[i] // Obtenemos cada peso por individual de la matriz de pesos (Obtendra un nuevo arreglo de 63 elementos)
+            for (let i = 0; i < arrWeights.length; i++) {
+                const weight = arrWeights[i] // Obtenemos cada peso por individual de la matriz de pesos (Obtendra un nuevo arreglo de 63 elementos)
                 const t = expected[i] // Cada item de t que es el valor esperado
                 let result = 0 // La sumatoria de los pesos por x que es la entrada, inicialmente sera 0
 
@@ -85,7 +97,8 @@ const entrenar = () => {
                     result += w * s // Va almacenando la sumatoria
                 }
 
-                result += weight[63] // A la sumatoria se le suma el bias
+                // result += weight[63] // A la sumatoria se le suma el bias
+                
 
                 // La funcion de activacion en este caso es la que determina el valor de y_in
                 if (activationFunc(result) !== t) {
@@ -103,28 +116,48 @@ const entrenar = () => {
         }
     }
     console.log({epoch})
+    letter.innerHTML = 'Entrenamiento exitoso 🎊'
 }
 
-// Repie el mismo procedimiento de aprendizaje omitiendo los pasos de actualizacion de los pesos y el biass
-const evaluar = () => {
-    output.length = 0
-    arrEntrada.length = []
-
-    for (let i of inputs) {
-        i.checked ? arrEntrada.push(1) : arrEntrada.push(-1)
+const conv = (arr) => {
+    const arrOut = []
+    for(let i in arr){
+        console.log(arr[i], arr[i] == -1)
+        arrOut[i] = (arr[i] == -1 || arr[i] == 0 ? 0 : 1)
     }
     
-    for(let i = 0; i < arrWeights.length; i++){
+    const letter = arrOut[2] * 1 + arrOut[1] * 2 + arrOut[0] * 4
+    const typography = arrOut[4] * 1 + arrOut[3] * 2 
+    return [letter, typography]
+}
+
+// Repite el mismo procedimiento de aprendizaje omitiendo los pasos de actualizacion de los pesos y el biass
+const evaluar = () => {
+    output.length = 0
+    inputArr.length = []
+
+    for (let i of inputs) {
+        (i.value ==='#' || i.value === '@') ? inputArr.push(1) : inputArr.push(-1);
+    }
+    
+    for (let i = 0; i < arrWeights.length; i++){
         let result = 0
-        for(let j = 0; j < arrEntrada.length; j++){
-            result +=  arrEntrada[j] * arrWeights[i][j];
+        for(let j = 0; j < inputArr.length; j++){
+            result +=  inputArr[j] * arrWeights[i][j];
         }
         
         // console.log({result})
         let y = activationFunc(result);
         output.push(y)
+        // console.log(inputArr)
+        console.log({arrWeights})
+        console.log({inputArr})
+        console.log(output)
     }
-    // console.log(output)
-    // console.log(weights)
-    letter.innerHTML = outputs[output.indexOf(1).toString()]
+
+    const letterType = outputs[conv(output)[0]]
+    const typographyType = conv(output)[1]
+
+    letter.innerHTML = (conv(output)[0] !== 0 && typographyType !== 0) ? (`Letra ${letterType} Tipografia ${typographyType}`) : 'Desconocido'
+    // letter.innerHTML = outputs[conv(output)[0]] + (conv(output)[1] == 0 ? ' ' : ' Tipografia: ' + conv(output)[1])
 }
--- js/pintar.js
+++ js/pintar.js
@@ -1,15 +1,20 @@
 const matriz = document.getElementById('matriz-entrada');
+const character = document.getElementById('type-character');
 
 const entrada = Array(63);
 
 for(let i = 0; i < entrada.length; i++){
     matriz.innerHTML += `
-        <input type='checkbox' class='inputs'>
+        <input type='text' class='inputs' readonly value=".">
     `
 }
 
+matriz.addEventListener('click', (e) => {
+    e.target.value = (character.value === '' || '#@O.'.indexOf(character.value) === -1) ? '.' : character.value 
+})
+
 const vaciar = () => {
     for(let i of inputs) {
-        i.checked = false;
+        i.value = '.';
     }
 }
